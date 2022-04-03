9c555efd87b642602b2524377dfc774d4ed39f70
Ronan Limon Duparcmeur <ronan.duparcmeur@lifen.fr>
2022-04-03 11:27:37 +0200

    Cleanup `#skill_rank_checkboxes` helper
    We're about to introduce different partials for different kind of skills,
    so their will be no need for the helper to interact with a `Skill` object.
    It will only need to know how many checkboxes to draw, and of what kind.

--- app/helpers/characters_helper.rb
+++ app/helpers/characters_helper.rb
@@ -1,10 +1,9 @@
 module CharactersHelper
-  # TODO: the number of ranks should be adjustable depending on the skill (ie. only 15 ranks for the Plate skill if the character wears a TA17)
-  def skill_rank_checkboxes(skill)
-    25.times.map do |n|
-      tag.label(class: class_names("skill-rank", { special: skill.special? })) do
+  def skill_rank_checkboxes(rank, max_rank: 25, special: false)
+    max_rank.times.map do |n|
+      tag.label(class: class_names("skill-rank", { special: special })) do
         tag.span(class: "skill-rank-input") do
-          tag.input(type: "checkbox", checked: (skill.ranks.to_i > n)) + tag.span(class: "skill-rank-control")
+          tag.input(type: "checkbox", checked: (rank.to_i > n)) + tag.span(class: "skill-rank-control")
         end
       end
     end.join.html_safe
