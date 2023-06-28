daca4a9d258fc5f35bcec538a87519aad6b28448
Ronan Limon Duparcmeur <ronan@2-45.pm>
2023-06-28 20:07:38 +0200

    Fix: use instantiated fixtures
    This setup must have been lost in a rebase…

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -11,6 +11,7 @@ class ActiveSupport::TestCase
 
   # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
   fixtures :all
+  self.use_instantiated_fixtures = true
 
   # Add more helper methods to be used by all tests here...
 end
